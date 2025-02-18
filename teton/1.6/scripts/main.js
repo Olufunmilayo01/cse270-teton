31934224bb18f68b8d367f4f9a5a74101ef798b1
Oluwafunmilayo01 <oluwafunmilayome@gmail.comgit config --list>
2025-02-18 21:33:53 +0100

    main added

--- teton/1.6/scripts/main.js
+++ teton/1.6/scripts/main.js
@@ -6,15 +6,15 @@ const weatherWind = document.querySelector("#windspeed");
 
 function displayResults(weatherData) {
   // You can use @2x or @4x to make the icon bigger, or omit it for the standard size
-  const iconsrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`
+  const iconsrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`;
   const desc = weatherData.weather[0].description;
   const main = weatherData.weather[0].main;
-  
-  weatherIcon.setAttribute('src', iconsrc);
-  weatherIcon.setAttribute('alt', desc);
-  weatherDesc.textContent = main; 
+
+  weatherIcon.setAttribute("src", iconsrc);
+  weatherIcon.setAttribute("alt", desc);
+  weatherDesc.textContent = main;
   weatherTemp.textContent = weatherData.main.temp.toFixed(0);
-  weatherWind.textContent = weatherData.wind.speed.toFixed(0);  
+  weatherWind.textContent = weatherData.wind.speed.toFixed(0);
 }
 
 async function getTheWeather() {
@@ -33,26 +33,25 @@ async function getTheWeather() {
 
 getTheWeather();
 
-function displaySpotlights(businessList){
-  spotlights = []
-  for (let i=0; i < 3; i++){
+function displaySpotlights(businessList) {
+  spotlights = [];
+  for (let i = 0; i < 3; i++) {
     spotlights.push(businessList[i]);
   }
 
-  // Now display stuff  
-  var mainspotlight = document.querySelector('.main-spotlight');
+  // Now display stuff
+  var mainspotlight = document.querySelector(".main-spotlight");
   spotlightcount = 1;
   spotlights.forEach((spotlight) => {
-    var newdiv = document.createElement('div');
-    newdiv.classList.add('spotlight'+spotlightcount);
+    var newdiv = document.createElement("div");
+    newdiv.classList.add("spotlight" + spotlightcount);
     spotlightcount++;
     newdiv.innerHTML = `<h4>${spotlight.name}</h4>
                         <p class="centered-image"><a href="${spotlight.websiteURL}"><img src="${spotlight.imageURL}"></a></p>
                         <p>${spotlight.streetAddress}, ${spotlight.cityStateZip}</p>
-                        <p>${spotlight.adcopy}</p>`
-    mainspotlight.append(newdiv);    
-  })
-
+                        <p>${spotlight.adcopy}</p>`;
+    mainspotlight.append(newdiv);
+  });
 }
 
 async function getBusinessData() {
@@ -67,18 +66,18 @@ async function getBusinessData() {
 
 getBusinessData();
 
-function doJoinFlag(){
-  let join_flag = localStorage.getItem("join_flag")
-  if (join_flag == null){
+function doJoinFlag() {
+  let join_flag = localStorage.getItem("join_flag");
+  if (join_flag == null) {
     return;
   }
-  document.querySelector(".join-original").style.display = 'none';    
-  document.querySelector(".join-new").style.display = 'none';
-  if (join_flag == 'enabled'){
-    document.querySelector(".join-new").style.display = 'block';    
+  document.querySelector(".join-original").style.display = "none";
+  document.querySelector(".join-new").style.display = "none";
+  if (join_flag == "enabled") {
+    document.querySelector(".join-new").style.display = "block";
   }
-  if (join_flag == 'disabled'){
-    document.querySelector(".join-original").style.display = 'block';
+  if (join_flag == "disabled") {
+    document.querySelector(".join-original").style.display = "block";
   }
 }
 
